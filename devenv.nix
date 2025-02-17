774b29a9b15b3cd09fa887766b25427883e6dbcc
0xthefr34k <tayayassine6@live.fr>
2025-02-17 11:28:33 -0500

    add abuseACL

--- devenv.nix
+++ devenv.nix
@@ -7,11 +7,12 @@
   languages.python = {
     uv.enable = true;
     enable = true;
-    venv.enable = true;
+    # venv.enable = true;
     venv.requirements = ''
      git+https://github.com/ShutdownRepo/shellerator
      git+https://github.com/0xthefr34k/pwncat-cs
      git+https://github.com/maurosoria/dirsearch
+     git+https://github.com/AetherBlack/abuseACL
     '';
     # libraries = [ pkgs.cairo ];
   };
